7bbb7e0915ec3f9a03a939803f50c851be873e54
Sanjeev Chauhan <145193048+Sanjeev-0312@users.noreply.github.com>
2025-06-05 18:51:04 +0530

    me

---
+++ Day-1/variable.js
@@ -0,0 +1,7 @@
+console.log(firstName, lastName, age, isLoggedIn, state, country);
+// Variable declaration
+const accountId ="1234567890";      //const value cannot be changed
+
+let accountEmail = "0312sanjeev@gmail.com"; //let value can be changed
+
+var accountPassword = "1234567890"; //var value can be changed
