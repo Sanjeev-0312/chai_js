9817d68b90d02b0400ae913d36a098fe4d3a2a86
Sanjeev-0312 <sanjeev@gmail.com>
2025-08-13 18:25:49 +0530

    me

--- Day-1/variable.js
+++ Day-1/variable.js
@@ -1,7 +1,12 @@
-console.log(firstName, lastName, age, isLoggedIn, state, country);
+console.log("firstName, lastName, age, isLoggedIn, state, country");
 // Variable declaration
 const accountId ="1234567890";      //const value cannot be changed
 
 let accountEmail = "0312sanjeev@gmail.com"; //let value can be changed
 
+
+
 var accountPassword = "1234567890"; //var value can be changed
+console.log(accountPassword);
+console.log(accountEmail);
+console.log(accountId);
